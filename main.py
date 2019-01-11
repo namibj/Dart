b9822c096604f93f229ff6cf80258e8898c262d0
Mehmet Ulrich <mehmetulrich@gmail.com>
2019-01-11 16:49:43 +0100

    funktionierende Wurfeingabe

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from table import table
 
 # TODO SpielModi (301, 501, 701)
 
+#class player:
+#	def __init__(self, name, score):
+#		self.name = name
+#		self.score = score
+
 
 def main():
 	score = 501
@@ -14,7 +19,6 @@ def main():
 		throw = 1
 		sum = 0
 		while throw < 4:
-
 			print("Wurf :{0}".format(throw))
 			x = input()
 			if x in table:
